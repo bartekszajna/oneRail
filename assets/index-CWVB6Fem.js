822f1aef0f8a352f98cb3ba262cfa297177ef3f9
Bartlomiej Szajna <baj@spyro-soft.com>
2025-10-26 23:30:25 +0100

    Updates

---
+++ assets/index-CWVB6Fem.js
@@ -0,0 +1,1 @@
+import{a as t,j as o}from"./index-Wq9lmydX.js";const a=()=>{const{id:s}=t();return o.jsxs("div",{children:["Hello product with id of ",s,"!"]})};export{a as default};
---
+++ assets/index-CqqfwhT9.js
@@ -0,0 +1,1 @@
+import{a as t,j as r}from"./index-Wq9lmydX.js";const o=()=>{const{id:s}=t();return r.jsxs("p",{children:["Edit: ID:",s]})};export{o as default};
