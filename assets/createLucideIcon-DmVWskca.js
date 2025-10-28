7382b9250257200a3886a162de933eb42553c017
Bartlomiej Szajna <baj@spyro-soft.com>
2025-10-28 14:39:15 +0100

    Updates

--- assets/createLucideIcon-DmVWskca.js
+++ assets/createLucideIcon-DmVWskca.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-DbYfRmLr.js";/**
+import{r as s}from"./index-CJ33uSs8.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.
