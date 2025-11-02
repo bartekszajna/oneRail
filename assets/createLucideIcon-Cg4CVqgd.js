c1568414d3d2a6a2f9509f5f9dfc0f0f762a0e21
Bartek Szajna <bartek.szajna@wp.pl>
2025-11-03 00:34:37 +0100

    Updates

--- assets/createLucideIcon-Cg4CVqgd.js
+++ assets/createLucideIcon-Cg4CVqgd.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-BIWb99zH.js";/**
+import{r as s}from"./index-DAiz0CAH.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ assets/index-BbVSVZam.js
@@ -0,0 +1,1 @@
+import{j as t,gr as e}from"./index-DAiz0CAH.js";const a=()=>t.jsx("main",{className:"min-h-screen my-16 mx-8",children:t.jsx(e,{})});export{a as default};
